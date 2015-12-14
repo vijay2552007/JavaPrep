93854f4738dff487e486c8f074d9162918c9534b
Vijay <vijay2552007@gmail.com>
2015-12-13 22:06:45 -0800

    JavaPrep

--- com/dynamicprog/KnapSackRecursive.java
+++ com/dynamicprog/KnapSackRecursive.java
@@ -1,22 +1,20 @@
 package com.dynamicprog;
 
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class KnapSackRecursive {
 
 	public static void main(String[] args) {
-		//int val[] = { 22, 20, 15, 30, 24, 54, 21, 32, 18, 25 };
-		//int wt[] = { 4, 2, 3, 5, 5, 6, 9, 7, 8, 10 };
-		
-		int val[] = { 2,4,6,9 };		
-		int wt[] = { 2,2,4,5};
+		int val[] = { 22, 20, 15, 30, 24, 54, 21, 32, 18, 25, 78, 34, 87, 90, 34 };
+		int wt[] = { 4, 2, 3, 5, 5, 6, 9, 7, 8, 10, 3, 4, 7, 9, 7 };
 
-		System.out.println("Recursive MaxValue for KnapSack == " + recursiveMaxValue(wt, val, 8));
+		System.out.println("\n\nRecursive MaxValue for KnapSack == " + recursiveMaxValue(wt, val, 30));
 
 	}
 
-	
-	//Override the key Object for HashMap so that equals and Hashcode are properly implemented...
+	// Override the key Object for HashMap so that equals and Hashcode are
+	// properly implemented...
 	public static class Index {
 		int remainingWt = 0;
 		int remainingItems = 0;
@@ -40,54 +38,130 @@ public class KnapSackRecursive {
 
 	}
 
+	// For BackTracking...
+	// Start from the RootNode...
+	// Left Node appear when the item is selected
+	// Right Node appear when not selected...
+	// Call recursively and cross compare the largest maxValue until you reach
+	// the bottom of the node tree...
+	private static class Node {
+		Node left = null;
+		Node right = null;
+
+		int maxValue = 0;
+		int weight = 0;
+	}
+
+	
+	
+	
+	
 	private static int recursiveMaxValue(int[] weights, int[] values, int w) {
 
-		HashMap<Index, Integer> mp = new HashMap<Index, Integer>();
-
-		return recursiveMaxValuePerform(weights, values, w, values.length, 0, mp);
+		HashMap<Index, Node> mpNode = new HashMap<Index, Node>();
+		Node rootNode = new Node();
+		rootNode.weight = weights[0];
+		Node nd = recursiveMaxValuePerform(weights, values, w, values.length, 0, rootNode, mpNode);
+		printSeq(rootNode);
+		return nd.maxValue;
 
 	}
 
-	      
 	
-	//This is a recursive call
-	private static int recursiveMaxValuePerform(int[] weightsArr, int[] valuesArr, int remainingWt, int remainingItems,
-			int currentItem, HashMap mp) {
+	
+	//Do Recursion on the given Items by selecting and not selecting.
+	//Built a Node tree while doimg this and store it in HashMap as Node object
+		private static Node recursiveMaxValuePerform(int[] weightsArr, int[] valuesArr, int remainingWt, int remainingItems,
+				int currentItem,Node nd, HashMap mp) {
 
-		// final return or recursive breaker here
-		if (remainingWt <= 0 || remainingItems <= 0)
-			return 0;
+			// final return or recursive breaker here
+			if (remainingWt <= 0 || remainingItems <= 0)
+				return new Node();
 
-		// Dynamic Programming part here
-		Index key = new Index();
-		key.remainingWt = remainingWt;
-		key.remainingItems = remainingItems;
+			
+			Index key = new Index();
+			key.remainingWt = remainingWt;
+			key.remainingItems = remainingItems;
 
-		if (mp.containsKey(key))
-			return (Integer) mp.get(key);
+			// Memoization part here
+			if (mp.containsKey(key))
+				return (Node) mp.get(key);
 
-		// Actual processing
-		//Kind of confusing..but let's try to decode here...
-		//Can get max value by either using the current item or not...
-		//so tree will have left and right sides here passing down recursively...
-		int maxValue = 0;
-		if (remainingWt >= weightsArr[currentItem]) {
-			maxValue = Math.max(
-					valuesArr[currentItem] + recursiveMaxValuePerform(weightsArr, valuesArr,
-							remainingWt - weightsArr[currentItem], remainingItems - 1, currentItem + 1, mp),
-					recursiveMaxValuePerform(weightsArr, valuesArr, remainingWt, remainingItems - 1, currentItem + 1,
-							mp));
-		} else {
+			
+			int maxValue = 0;
+			if (remainingWt >= weightsArr[currentItem]) {
+				Node leftNodeBySelecting = recursiveMaxValuePerform(weightsArr, valuesArr,
+						remainingWt - weightsArr[currentItem], remainingItems - 1, currentItem + 1,new Node(), mp);
+				
+				Node rightNodeByNotSelecting = recursiveMaxValuePerform(weightsArr, valuesArr, remainingWt, remainingItems - 1, currentItem + 1,
+						new Node(),mp);
+				
+				
+				if(leftNodeBySelecting.maxValue + valuesArr[currentItem] > rightNodeByNotSelecting.maxValue)
+				{
+					nd.maxValue = valuesArr[currentItem] + leftNodeBySelecting.maxValue;
+					nd.left = leftNodeBySelecting;//Left Node always formed by Selecting Item
+				}
+				else
+				{
+					nd.maxValue = rightNodeByNotSelecting.maxValue;
+					nd.right = rightNodeByNotSelecting;
+				}
+				
+				
+			} else {
+				
+				Node rightNodeByNotSelecting = recursiveMaxValuePerform(weightsArr, valuesArr, remainingWt, remainingItems - 1, currentItem + 1,
+						new Node(),mp);
+				nd.maxValue = rightNodeByNotSelecting.maxValue;
+				nd.right = rightNodeByNotSelecting;
 
-			maxValue = recursiveMaxValuePerform(weightsArr, valuesArr, remainingWt, remainingItems - 1, currentItem + 1,
-					mp);
+				
+			}			
+			
+			//Store key value pair so that can be used later..Dynamic programming applied here..
+			nd.weight = weightsArr[currentItem];//Record current weight for node so that can be printed later...
+			mp.put(key, nd);
+
+			return nd;
 		}
 		
 		
-		//Store key value pair so that can be used later..Dynamic programming concept applied here..
-		mp.put(key, maxValue);
 
-		return maxValue;
+	// This method will do DFS[i.e. Depth First Search] on tree node by using recursion and will print
+	// values only if max value is on Left node i.e. Item is picked...
+	private static void printSeq(Node nd) {
+		if (nd.right == null) {
+			if (nd.left == null) {
+				// Do Nothing
+				return;
+			} else {
+				System.out.print(nd.weight + ",");// As left node selected, print
+													// it on screen...
+				printSeq(nd.left);
+				return;
+			}
+		}
+
+		if (nd.left == null) {
+			if (nd.right == null) {
+				// Do Nothing
+				return;
+			} else {
+				printSeq(nd.right);
+				return;
+			}
+		}
+
+		if (nd.left.maxValue  > nd.right.maxValue) {
+			System.out.print(nd.weight + ",");// As left node selected, print it
+												// on screen...
+			printSeq(nd.left);
+
+		} else {
+			printSeq(nd.right);
+		}
+
 	}
 
 }
